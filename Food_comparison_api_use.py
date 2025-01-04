71867937bef9ae4857e61483c961e602b786829d
Abraham LeFevre <159225028+Abrahaml12@users.noreply.github.com>
2025-01-04 18:03:40 -0500

    Update Food_comparison_api_use.py

--- Food_comparison_api_use.py
+++ Food_comparison_api_use.py
@@ -1,3 +1,6 @@
+# The medium article associated with this code is here: https://medium.com/inst414-data-science-tech/pancakes-french-toast-or-waffles-which-is-the-best-7c82e146a0b7
+
+
 import requests
 import pandas as pd
 
